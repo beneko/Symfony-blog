1c73d0ef1d455cc175cf7d4657adc3c47befc194
beneko <behzadnekouei@gmail.com>
2022-02-25 13:48:42 +0100

    add article form created and tested

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class BlogController extends AbstractController
 {
@@ -27,6 +30,30 @@ class BlogController extends AbstractController
     public function home(){
         return $this->render('blog/home.html.twig',[
             
+        ]);
+    }
+
+    /**
+     * @Route("blog/new", name="blog_new")
+     */
+    public function create(Request $request,ManagerRegistry $doctrine): Response {
+
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $article->setCreatedAt(NEW \DateTimeImmutable());
+            $article = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()] );
+        }
+        return $this->render('blog/create.html.twig',[
+            'formArticle' => $form->createView()
         ]);
     }
 
